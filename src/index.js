9f5056345b33f0fe42777b5935ba71bb38430308
ALexandrLaza <lazarforcode@gmail.com>
2021-03-11 16:27:55 +0300

    feat: add function code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    let numb = String(n).split("").reverse();
+    if(numb[numb.length-1]== "+" || numb[numb.length-1]== "-"){
+        numb.pop();
+    }
+    return numb.join("");
 }
